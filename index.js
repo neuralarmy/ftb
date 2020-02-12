4ec1032dea65b84abf52cc52f5ce70d1b7bf08a4
Yasin-Rahman <yasinshreshto@gmail.com>
2020-02-12 23:10:05 +0600

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // parse requests of content-type - application/json
 app.use(bodyParser.json())
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
